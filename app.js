c813180ca4eb809b8dca7c019de63076f97177af
morkovka3 <ashleythain420@gmail.com>
2022-09-15 22:16:41 +0100

    just to push to git, 1 key sound again

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
+
 const demoKey = document.getElementById("1");
 console.log(demoKey);
 
+// function keySound(key_id) {
+//     playNote = new Audio(`24-piano-notes/key${key_id}.mp3`);
+//     playNote.play();
+// }
+demoKey.addEventListener('click', keySound);
+
 function keySound() {
-    playNote = new Audio('24-piano-notes/key01.mp3');
+    playNote = new Audio('24-piano-notes/key1.mp3');
     playNote.play();
 }
 
-demoKey.addEventListener('click', keySound);
