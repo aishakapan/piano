d5e85bd7fe9069a6ef6ef8960a17dd1dedae30d0
morkovka3 <ashleythain420@gmail.com>
2022-09-19 14:24:45 +0100

    added sound to all keys

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 
+const allKeys = document.querySelectorAll('.key');
 
-const demoKey = document.getElementById("1");
-console.log(demoKey);
 
-// function keySound(key_id) {
-//     playNote = new Audio(`24-piano-notes/key${key_id}.mp3`);
-//     playNote.play();
-// }
-demoKey.addEventListener('click', keySound);
-
-function keySound() {
-    playNote = new Audio('24-piano-notes/key1.mp3');
-    playNote.play();
+for (const key of allKeys) {
+    key.addEventListener('click', () => {
+        playNote = new Audio(`24-piano-notes/key${key.id}.mp3`);
+        playNote.play();
+    });
 }
 
